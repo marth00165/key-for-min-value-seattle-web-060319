4117e7ee3804321b86b83c1ad5d79bf012c7a551
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-18 02:45:29 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+min_Val = nil
+min_Key = nil
+
+name_hash.each do |key, value|
+  
+  if value < min_Val || min_Val == nil
+    min_Val = value
+    min_Key = key
+  end
 end
+
+return min_Key
+
+end
+
+key_for_min_value(name_hash)
