9bd9a6da96fe45fee76c78cd06ce51fc47359710
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-18 02:44:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+min_Val = nil
+min_Key = nil
+
+name_hash.each do |key, value|
+  
+  if value < min_Val || min_Val == nil
+    min_Val = value
+    min_Key = key
+  end
 end
+
+return min_Key
+
+end
+
